84184ad5c4e83ce2a4d3b9936a2de537476ce03a
M.H. Rafi <m.h.rafi436@gmail.com>
2026-04-09 03:48:27 +0600

    initial commit

---
+++ src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/ResortManagement/ManageRoomPricingController.java
@@ -0,0 +1,21 @@
+package com.spring26.section2.group19.ecoresort.SamihaJahanSaaj.ResortManagement;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ManageRoomPricingController
+{
+    @javafx.fxml.FXML
+    private TextField priceField;
+    @javafx.fxml.FXML
+    private TableView pricingTable;
+    @javafx.fxml.FXML
+    private ComboBox roomTypeBox;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
--- src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/ResortManagement/ViewoverallresortperformanceController.java
+++ src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/ResortManagement/ViewoverallresortperformanceController.java
@@ -1,7 +1,19 @@
 package com.spring26.section2.group19.ecoresort.SamihaJahanSaaj.ResortManagement;
 
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
 public class ViewoverallresortperformanceController
 {
+    @javafx.fxml.FXML
+    private Label bookingsLabel;
+    @javafx.fxml.FXML
+    private Label revenueLabel;
+    @javafx.fxml.FXML
+    private Label occupancyLabel;
+    @javafx.fxml.FXML
+    private DatePicker datePicker;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
