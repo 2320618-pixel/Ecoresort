a21013561b83e2119818e5b2eba08b4dec3a9a91
takee2211175 <2211175@iub.edu.bd>
2026-04-08 23:35:41 +0600

    created fxml created Controller update

---
+++ src/main/java/com/spring26/section2/group19/ecoresort/farzana/workAsCompletedController.java
@@ -0,0 +1,33 @@
+package com.spring26.section2.group19.ecoresort.farzana;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class workAsCompletedController
+{
+    @javafx.fxml.FXML
+    private TableColumn validatesCompletionColumn;
+    @javafx.fxml.FXML
+    private TableView TourTable;
+    @javafx.fxml.FXML
+    private TableColumn completionColumn;
+    @javafx.fxml.FXML
+    private TableColumn ShowsConfirmationColumn;
+    @javafx.fxml.FXML
+    private TableColumn confirmationColumn;
+    @javafx.fxml.FXML
+    private TableView markTable;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void rejectButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acceptButton(ActionEvent actionEvent) {
+    }
+}
