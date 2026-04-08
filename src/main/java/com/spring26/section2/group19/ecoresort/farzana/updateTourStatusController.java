591ae520dfca866a851aefc9a3db6d9fa049c09a
takee2211175 <2211175@iub.edu.bd>
2026-04-08 20:47:11 +0600

    created fxml created Controller update

---
+++ src/main/java/com/spring26/section2/group19/ecoresort/farzana/updateTourStatusController.java
@@ -0,0 +1,36 @@
+package com.spring26.section2.group19.ecoresort.farzana;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class updateTourStatusController
+{
+    @javafx.fxml.FXML
+    private TableColumn statusInputColumn;
+    @javafx.fxml.FXML
+    private TableView tourTable;
+    @javafx.fxml.FXML
+    private Button rejectButton;
+    @javafx.fxml.FXML
+    private TableView updatetourstatustable;
+    @javafx.fxml.FXML
+    private TableColumn PersonColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void updateTourStatusTabButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void rejectButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void acceptButton(ActionEvent actionEvent) {
+    }
+}
