828063662658d3a3a75f282d6a110272a28fdabe
takee2211175 <2211175@iub.edu.bd>
2026-04-11 01:45:27 +0600

    created fxml created Controller update

---
+++ src/main/java/com/spring26/section2/group19/ecoresort/RecordGuests.java
@@ -0,0 +1,35 @@
+package com.spring26.section2.group19.ecoresort;
+
+public class RecordGuests {
+    private String Guests;
+    private String Attendance;
+
+    public RecordGuests(String guests, String attendance) {
+        Guests = guests;
+        Attendance = attendance;
+    }
+
+    public String getAttendance() {
+        return Attendance;
+    }
+
+    public void setAttendance(String attendance) {
+        Attendance = attendance;
+    }
+
+    public String getGuests() {
+        return Guests;
+    }
+
+    public void setGuests(String guests) {
+        Guests = guests;
+    }
+
+    @Override
+    public String toString() {
+        return "RecordGuests{" +
+                "Guests='" + Guests + '\'' +
+                ", Attendance='" + Attendance + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/spring26/section2/group19/ecoresort/farzana/maintenanceIssuesFacilitiesController.java
+++ src/main/java/com/spring26/section2/group19/ecoresort/farzana/maintenanceIssuesFacilitiesController.java
@@ -1,28 +1,38 @@
 package com.spring26.section2.group19.ecoresort.farzana;
 
 import com.spring26.section2.group19.ecoresort.HelloApplication;
+import com.spring26.section2.group19.ecoresort.Maintenance;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class maintenanceIssuesFacilitiesController
 {
     @javafx.fxml.FXML
-    private TableColumn IssueTypeColumn;
+    private TableColumn<Maintenance, String> IssueTypeColumn;
     @javafx.fxml.FXML
-    private TableColumn savesinputColumn;
+    private TableColumn<Maintenance, String> savesinputColumn;
     @javafx.fxml.FXML
-    private TableColumn stuffLocationColumn;
+    private TableColumn<Maintenance, String> stuffLocationColumn;
     @javafx.fxml.FXML
-    private TableView maintenanceTable;
+    private TableView<Maintenance> maintenanceTable;
+    List<Maintenance> maintenanceList = new ArrayList<>();
 
     @javafx.fxml.FXML
     public void initialize() {
+        IssueTypeColumn.setCellValueFactory(new PropertyValueFactory<>("issuesType"));
+        savesinputColumn.setCellValueFactory(new PropertyValueFactory<>("savesInput"));
+        stuffLocationColumn.setCellValueFactory(new PropertyValueFactory<>("StuffLocation"));
+
+        maintenanceList.add(new Maintenance("Dhaka", "no Ac", "yes"));
     }
 
     @javafx.fxml.FXML
@@ -31,6 +41,7 @@ public class maintenanceIssuesFacilitiesController
 
     @javafx.fxml.FXML
     public void maintenanceIssuesFacilitiesTabButton(ActionEvent actionEvent) {
+        maintenanceTable.getItems().addAll(maintenanceList);
     }
 
     @javafx.fxml.FXML
