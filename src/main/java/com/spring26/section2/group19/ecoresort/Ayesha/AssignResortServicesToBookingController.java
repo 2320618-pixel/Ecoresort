3531586620f9a1b35c6a2175c0d89ecc560b38bc
Ayesha-IUb <2211155@iub.edu.bd>
2026-04-11 20:24:13 +0600

    ayesha created AssignResortServicesToBooking.fxml and AssignResortServicesToBookingController.java

---
+++ src/main/java/com/spring26/section2/group19/ecoresort/Ayesha/AssignResortServicesToBookingController.java
@@ -0,0 +1,44 @@
+package com.spring26.section2.group19.ecoresort.Ayesha;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class AssignResortServicesToBookingController
+{
+    @javafx.fxml.FXML
+    private ComboBox serviceType;
+    @javafx.fxml.FXML
+    private TableColumn serviceTypeClm;
+    @javafx.fxml.FXML
+    private RadioButton normal;
+    @javafx.fxml.FXML
+    private TableColumn priorityClm;
+    @javafx.fxml.FXML
+    private TextField filterPriority;
+    @javafx.fxml.FXML
+    private TableView tableView;
+    @javafx.fxml.FXML
+    private TextArea noteArea;
+    @javafx.fxml.FXML
+    private TableColumn noteClm;
+    @javafx.fxml.FXML
+    private RadioButton urgent;
+    @javafx.fxml.FXML
+    private TableColumn bookingIDClm;
+    @javafx.fxml.FXML
+    private TextField bookingID;
+    @javafx.fxml.FXML
+    private ToggleGroup radio;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void filterBtn(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assignServiceOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/spring26/section2/group19/ecoresort/Ayesha/ServiceAssignment.java
@@ -0,0 +1,5 @@
+package com.spring26.section2.group19.ecoresort.Ayesha;
+
+public class ServiceAssignment {
+    public
+}
