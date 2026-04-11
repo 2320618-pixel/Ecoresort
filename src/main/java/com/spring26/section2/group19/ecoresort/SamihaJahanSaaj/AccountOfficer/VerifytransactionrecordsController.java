fbd3415a0d359fca279911327baf29ea73fe3a8b
M.H. Rafi <2432084@iub.edu.bd>
2026-04-11 17:02:02 +0600

    Saaj fxml

--- src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/AccountOfficer/VerifytransactionrecordsController.java
+++ src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/AccountOfficer/VerifytransactionrecordsController.java
@@ -1,7 +1,30 @@
 package com.spring26.section2.group19.ecoresort.SamihaJahanSaaj.AccountOfficer;
 
+import javafx.scene.control.*;
+
 public class VerifytransactionrecordsController
 {
+    @javafx.fxml.FXML
+    private Button resetBtn;
+    @javafx.fxml.FXML
+    private Button refreshBtn;
+    @javafx.fxml.FXML
+    private TableView transactionTable;
+    @javafx.fxml.FXML
+    private ComboBox statusBox;
+    @javafx.fxml.FXML
+    private ComboBox methodBox;
+    @javafx.fxml.FXML
+    private TextField guestNameField;
+    @javafx.fxml.FXML
+    private DatePicker datePicker;
+    @javafx.fxml.FXML
+    private TextField transactionIdField;
+    @javafx.fxml.FXML
+    private TextField amountField;
+    @javafx.fxml.FXML
+    private Button saveBtn;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
