461a98a79b59635ee16cced81d56411f86a1c384
2432084-max <2432084@iub.edu.bd>
2026-04-11 23:13:21 +0600

    Saaj fxml

--- src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/ResortManagement/HandleguestscomplainsController.java
+++ src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/ResortManagement/HandleguestscomplainsController.java
@@ -1,4 +1,31 @@
 package com.spring26.section2.group19.ecoresort.SamihaJahanSaaj.ResortManagement;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class HandleguestscomplainsController {
+    @javafx.fxml.FXML
+    private TableColumn colIssue;
+    @javafx.fxml.FXML
+    private ComboBox staffComboBox;
+    @javafx.fxml.FXML
+    private TableColumn colGuest;
+    @javafx.fxml.FXML
+    private TableView complaintTable;
+    @javafx.fxml.FXML
+    private TextArea complaintDetailsArea;
+    @javafx.fxml.FXML
+    private TableColumn colId;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private TableColumn colStatus;
+
+    @javafx.fxml.FXML
+    public void handleResolveComplaint(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAssignComplaint(ActionEvent actionEvent) {
+    }
 }
