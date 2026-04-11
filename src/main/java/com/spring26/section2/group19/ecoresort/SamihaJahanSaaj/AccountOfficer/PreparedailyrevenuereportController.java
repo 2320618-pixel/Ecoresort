b090619d98467ed7c8cffacad90e05d89dff51da
M.H. Rafi <2432084@iub.edu.bd>
2026-04-11 16:48:55 +0600

    Saaj fxml

--- src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/AccountOfficer/PreparedailyrevenuereportController.java
+++ src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/AccountOfficer/PreparedailyrevenuereportController.java
@@ -1,7 +1,30 @@
 package com.spring26.section2.group19.ecoresort.SamihaJahanSaaj.AccountOfficer;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class PreparedailyrevenuereportController
 {
+    @javafx.fxml.FXML
+    private Button resetBtn;
+    @javafx.fxml.FXML
+    private Button refreshBtn;
+    @javafx.fxml.FXML
+    private Button updateBtn;
+    @javafx.fxml.FXML
+    private TableView transactionTable;
+    @javafx.fxml.FXML
+    private TextField methodField;
+    @javafx.fxml.FXML
+    private TextField guestNameField;
+    @javafx.fxml.FXML
+    private TextField dateField;
+    @javafx.fxml.FXML
+    private TextField transactionIdField;
+    @javafx.fxml.FXML
+    private TextField amountField;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
