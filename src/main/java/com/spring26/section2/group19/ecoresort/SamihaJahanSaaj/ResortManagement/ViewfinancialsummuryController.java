8f75bfab1f26034716892c23248b5859cdbb4d8f
2432084-max <2432084@iub.edu.bd>
2026-04-11 23:58:09 +0600

    Saaj fxml

--- src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/ResortManagement/ViewfinancialsummuryController.java
+++ src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/ResortManagement/ViewfinancialsummuryController.java
@@ -1,7 +1,25 @@
 package com.spring26.section2.group19.ecoresort.SamihaJahanSaaj.ResortManagement;
 
+import javafx.event.ActionEvent;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ViewfinancialsummuryController
 {
     @javafx.fxml.FXML
+    private Label expenseLabel;
+    @javafx.fxml.FXML
+    private Label profitLabel;
+    @javafx.fxml.FXML
+    private Label incomeLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleRefreshData(ActionEvent actionEvent) {
+    }
+}
