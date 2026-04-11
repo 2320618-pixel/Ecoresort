c002abe4a99864e8d66c9ddb6a01fe9afcee1338
Ayesha-IUb <2211155@iub.edu.bd>
2026-04-11 22:53:07 +0600

    ayesha add UpdateOrCancelBookingRequest FXML and controller; implement UI elements and functionality for updating or canceling booking requests

---
+++ src/main/java/com/spring26/section2/group19/ecoresort/Ayesha/UpdateOrCancelBookingRequestController.java
@@ -0,0 +1,31 @@
+package com.spring26.section2.group19.ecoresort.Ayesha;
+
+public class UpdateOrCancelBookingRequestController
+{
+    @javafx.fxml.FXML
+    private TableColumn dateClm;
+    @javafx.fxml.FXML
+    private DatePicker updateDate;
+    @javafx.fxml.FXML
+    private TextField updateGuestNo;
+    @javafx.fxml.FXML
+    private TextArea updateRequirements;
+    @javafx.fxml.FXML
+    private TableColumn requirementsClm;
+    @javafx.fxml.FXML
+    private TableView tableView;
+    @javafx.fxml.FXML
+    private TableColumn noOfGuestClm;
+    @javafx.fxml.FXML
+    private Button updateReqBtn;
+    @javafx.fxml.FXML
+    private Button cancelreqBtn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void saveBtn(ActionEvent actionEvent) {
+    }
+}
