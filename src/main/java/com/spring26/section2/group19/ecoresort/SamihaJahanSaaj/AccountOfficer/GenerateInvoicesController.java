c20b545b739cf94927149c0af7531a6d4f559457
M.H. Rafi <2432084@iub.edu.bd>
2026-04-11 16:19:42 +0600

    Saaj fxml

--- src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/AccountOfficer/GenerateInvoicesController.java
+++ src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/AccountOfficer/GenerateInvoicesController.java
@@ -1,7 +1,35 @@
 package com.spring26.section2.group19.ecoresort.SamihaJahanSaaj.AccountOfficer;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class GenerateInvoicesController
 {
+    @javafx.fxml.FXML
+    private TextField roomChargeField;
+    @javafx.fxml.FXML
+    private Button printBtn;
+    @javafx.fxml.FXML
+    private Button loadBtn;
+    @javafx.fxml.FXML
+    private TextField guestNameField;
+    @javafx.fxml.FXML
+    private TextField taxField;
+    @javafx.fxml.FXML
+    private TextField totalField;
+    @javafx.fxml.FXML
+    private TextField serviceChargeField;
+    @javafx.fxml.FXML
+    private Button downloadBtn;
+    @javafx.fxml.FXML
+    private Button generateBtn;
+    @javafx.fxml.FXML
+    private ComboBox bookingComboBox;
+    @javafx.fxml.FXML
+    private TextArea invoicePreviewArea;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
--- src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/AccountOfficer/RecordguestpaymentsController.java
+++ src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/AccountOfficer/RecordguestpaymentsController.java
@@ -1,7 +1,28 @@
 package com.spring26.section2.group19.ecoresort.SamihaJahanSaaj.AccountOfficer;
 
+import javafx.scene.control.*;
+
 public class RecordguestpaymentsController
 {
+    @javafx.fxml.FXML
+    private ComboBox paymentMethodBox;
+    @javafx.fxml.FXML
+    private TextArea referenceField;
+    @javafx.fxml.FXML
+    private Button clearBtn;
+    @javafx.fxml.FXML
+    private Button cancelBtn;
+    @javafx.fxml.FXML
+    private TableView paymentTable;
+    @javafx.fxml.FXML
+    private TextField guestNameField;
+    @javafx.fxml.FXML
+    private DatePicker paymentDate;
+    @javafx.fxml.FXML
+    private TextField amountField;
+    @javafx.fxml.FXML
+    private Button saveBtn;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
