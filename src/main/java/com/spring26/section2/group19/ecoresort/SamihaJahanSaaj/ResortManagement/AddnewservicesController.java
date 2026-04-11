c0b7b5073a162c5a471f726fb82447b48b7afc0b
2432084-max <2432084@iub.edu.bd>
2026-04-11 23:25:20 +0600

    Saaj fxml

--- src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/ResortManagement/AddnewservicesController.java
+++ src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/ResortManagement/AddnewservicesController.java
@@ -1,7 +1,29 @@
 package com.spring26.section2.group19.ecoresort.SamihaJahanSaaj.ResortManagement;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class AddnewservicesController
 {
     @javafx.fxml.FXML
+    private Label validationLabel;
+    @javafx.fxml.FXML
+    private TextField serviceNameField;
+    @javafx.fxml.FXML
+    private TextField servicePriceField;
+    @javafx.fxml.FXML
+    private Label successLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleSaveService(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleClearForm(ActionEvent actionEvent) {
+    }
+}
