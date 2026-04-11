2414e32aab06f7182518206d39f0ccd616c235f6
M.H. Rafi <2432084@iub.edu.bd>
2026-04-11 16:54:52 +0600

    Saaj fxml

--- src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/AccountOfficer/ExportfinancialdataController.java
+++ src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/AccountOfficer/ExportfinancialdataController.java
@@ -1,7 +1,28 @@
 package com.spring26.section2.group19.ecoresort.SamihaJahanSaaj.AccountOfficer;
 
+import javafx.scene.control.*;
+
 public class ExportfinancialdataController
 {
+    @javafx.fxml.FXML
+    private Button resetBtn;
+    @javafx.fxml.FXML
+    private DatePicker fromDatePicker;
+    @javafx.fxml.FXML
+    private RadioButton pdfOption;
+    @javafx.fxml.FXML
+    private DatePicker toDatePicker;
+    @javafx.fxml.FXML
+    private RadioButton excelOption;
+    @javafx.fxml.FXML
+    private ComboBox dataTypeBox;
+    @javafx.fxml.FXML
+    private Button downloadBtn;
+    @javafx.fxml.FXML
+    private Button generateBtn;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}
