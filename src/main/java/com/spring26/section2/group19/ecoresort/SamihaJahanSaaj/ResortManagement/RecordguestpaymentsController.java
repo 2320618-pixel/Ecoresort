5fce345b6c9428099137d914c3a60b05258bf991
2432084-max <2432084@iub.edu.bd>
2026-04-12 00:13:59 +0600

    Saaj fxml

--- src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/ResortManagement/RecordguestpaymentsController.java
+++ src/main/java/com/spring26/section2/group19/ecoresort/SamihaJahanSaaj/ResortManagement/RecordguestpaymentsController.java
@@ -1,7 +1,32 @@
 package com.spring26.section2.group19.ecoresort.SamihaJahanSaaj.ResortManagement;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class RecordguestpaymentsController
 {
     @javafx.fxml.FXML
+    private TextField guestNameField;
+    @javafx.fxml.FXML
+    private DatePicker datePicker;
+    @javafx.fxml.FXML
+    private Label successLabel;
+    @javafx.fxml.FXML
+    private Label errorLabel;
+    @javafx.fxml.FXML
+    private TextField amountField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleClearForm(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSavePayment(ActionEvent actionEvent) {
+    }
+}
